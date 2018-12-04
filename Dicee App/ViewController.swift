4f0111f5f69bef2f816d095c63c2cea9bf8dfae9
Rave.N.Holm <barakylie@gmail.com>
2018-12-03 22:50:10 -0500

    Initial Commit

---
+++ Dicee App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee App
+//
+//  Created by Kylie Bara (Raven Blackwell) on 12/3/18.
+//  Copyright © 2018 Kylie Bara (Raven Blackwell). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
