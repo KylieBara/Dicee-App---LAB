becdb359a8c255a589c58359843ea43b278561e4
Rave.N.Holm <barakylie@gmail.com>
2018-12-04 20:59:06 -0500

    - Attached IBOutlets - Created Dice Index Variables - Created random number generator.

--- Dicee App/ViewController.swift
+++ Dicee App/ViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
 
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    // Wave if you see this.
-
+    @IBAction func throwButtonPressed(_ sender: Any) {
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
+        
+        
+        
+    }
+    
+    
 }
 
